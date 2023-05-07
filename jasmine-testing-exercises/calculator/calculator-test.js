ccaa93b8b1d2a054da7cd52b291cf8d8960eca28
Schartelb <106553010+Schartelb@users.noreply.github.com>
2023-05-07 11:24:27 -0400

    Add files via upload

---
+++ jasmine-testing-exercises/calculator/calculator-test.js
@@ -0,0 +1,12 @@
+
+it('should calculate the monthly rate correctly', function () {
+  let payment = (document.getElementById("monthly-payment"))
+  expect(update())
+});
+
+
+it("should return a result with 2 decimal places", function () {
+  // ..
+});
+
+/// etc
